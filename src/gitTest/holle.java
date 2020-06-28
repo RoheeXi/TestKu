3fc15cc39d37529e45d94a073024f6952a5d9ead
RoheeXi <123456@qq.com>
2020-06-28 10:09:55 +0800

    iss53第一次修改提交

--- src/gitTest/holle.java
+++ src/gitTest/holle.java
@@ -5,6 +5,7 @@ public class holle {
 	public static void main(String[] args) {
 		System.out.println("master本身代码");
         System.out.println("master第一次修改");
+        System.out.println("iss53错误修改中");
 	}
 
 }
